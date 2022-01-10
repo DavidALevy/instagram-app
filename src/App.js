010449b0c8a67fb8f0ad006fe20aebbbd75e1941
DavidALevy <david.levy.levy@btinternet.com>
2022-01-10 19:04:29 +0000

    improvenents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css' ;
+import { useEffect, useState } from "react";
+ 
 
-function App() {
+const App = () => {
+  const [item, setitem] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const handler = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://picsum.photos/v2/list?page=2&limit=10");
+      console.log(response);
+      console.log("fetching")
+      if (response.status !== 200) {
+        throw new Error("not fetching");
+      }
+      const data = await response.json();   
+setitem(data)
+      console.log("API info", data);
+      setLoading(false);
+    } catch (e) {
+      setError({ error: true, message: e.message });
+    }
+  };
+
+  useEffect(() => {
+    handler();
+  }, []);
+
+  if (error.error) {
+    return <h1>{error.message}</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <div className='main'>
+         <header className="title"><h1 >Instagram Photos</h1></header>
+           {item.map((item, index) => {
+        return  <img className='images'key={index} src={item.download_url}alt="instagram"/>
+        })}
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
